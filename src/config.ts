7d2681418e71ff8f33d109603b3e9901898eb767
thestudydiaries <thestudydiaries@proton.me>
2026-02-20 16:39:12 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,10 +14,10 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://studydiaries.netlify.app/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Bangkok",
 } as const;
